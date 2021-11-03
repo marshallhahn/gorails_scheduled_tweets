ebbd11fb498a09ef20c46992fe0380569fa8b179
Marshall Hahn <git@marshallhahn.com>
2021-11-03 18:17:40 -0500

    Part 13

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@
 Rails.application.routes.draw do
   get "about", to: "about#index", as: :about
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
   root 'main#index'
 end
