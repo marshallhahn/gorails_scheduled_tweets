65f39ee7a41910f4a9cc0de8bb2984c77eb8e7c6
Marshall Hahn <git@marshallhahn.com>
2021-11-04 09:12:17 -0500

    Part 25

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,19 @@
 class OmniAuthCallbacksController < ApplicationController
   def twitter
-    render plain: "Success!"
+    twitter_account = Current.user.twitter_accounts.where(username: auth.info.nickname).first_or_initialize
+    twitter_account.update(
+      name: auth.info.name,
+      avatar: auth.info.image,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret
+    )
+
+    redirect_to root_path, notice: "Successfully connected twitter account."
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
