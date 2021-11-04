b611028bcdf245469a0cb7f461c5ddabf9e5860a
Marshall Hahn <git@marshallhahn.com>
2021-11-03 21:27:56 -0500

    Part 18

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,9 +1,4 @@
 class MainController < ApplicationController
   def index
-    if session[:user_id]
-      @user = User.find_by(id: session[:user_id])
-    end
-    # flash[:notice] = 'Logged in'
-    # flash[:alert] = 'Invalid email or password'
   end
 end
