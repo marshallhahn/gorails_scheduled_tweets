9822c9c3505984cc4d84e2f9d2c46d5655373ff6
Marshall Hahn <git@marshallhahn.com>
2021-11-03 22:42:16 -0500

    Part 22

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -12,4 +12,25 @@ class PasswordResetsController < ApplicationController
     redirect_to root_path, notice: "If an account with that email was found, a password reset email has been sent."
   end
 
+  def edit
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to sign_in_path, alert: "Your token has expired. Please try again."
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password_reset")
+    if @user.update(password_params)
+      redirect_to sign_in_path, notice: "Password was reset. Please log in."
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
 end
